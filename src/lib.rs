2eff22b5d2505657af00d41297aa750dc8b00f9c
us157t <us157t@126.com>
2024-10-31 16:31:31 +0800

    feat: Add post

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,29 @@ use actix_web::{
 use std::net::TcpListener;
 use actix_web::dev::Server;
 
+
+#[derive(serde::Deserialize)]
+struct FD {
+	name: String
+}
+
 async fn greet() -> impl Responder {
 	HttpResponse::Ok()
+}
+
+async fn post_200(_form: web::Form<FD>) -> HttpResponse {
+	HttpResponse::Ok().finish()
+}
+
+fn index(form: web::Form<FD>) -> String {
+	format!("Welcome {}", form.name)
 }
 
 pub fn run(lis: TcpListener) -> Result<Server, std::io::Error> {
 	let s = HttpServer::new(|| {
 		App::new()
 			.route("/greet", web::get().to(greet))
+			.route("/post_200", web::post().to(post_200))
 	})
 	.listen(lis)?
 	.run();
--- tests/hc.rs
+++ tests/hc.rs
@@ -16,6 +16,22 @@ async fn hc_works() {
 		
 }
 
+#[tokio::test]
+async fn post_200() {
+	let app_addr = spawn_app();
+	let cli = reqwest::Client::new();
+	let body = "name=le%20guin";
+	let res = cli
+		.post(&format!("{}/post_200", &app_addr))
+		.header("Content-Type", "application/x-www-form-urlencoded")
+		.body(body)
+		.send()
+		.await
+		.expect("post_200 err");
+
+	assert_eq!(200, res.status().as_u16());
+}
+
 fn spawn_app()  -> String{
 	let lis = TcpListener::bind("127.0.0.1:0")
 		.expect("Failed to bind ");
